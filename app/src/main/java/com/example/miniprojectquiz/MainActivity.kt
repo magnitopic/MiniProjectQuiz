caa07d54e851737af6583801347293c85ce9889a
magnitopic <magnitopic@gmail.com>
2023-02-06 17:15:33 +0100

    second activity design finished

--- app/src/main/java/com/example/miniprojectquiz/MainActivity.kt
+++ app/src/main/java/com/example/miniprojectquiz/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.miniprojectquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun starQuiz(vista: View) {
+        val i = Intent(this, checkbox::class.java)
+        startActivity(i)
     }
 }
--- app/src/main/java/com/example/miniprojectquiz/checkbox.kt
+++ app/src/main/java/com/example/miniprojectquiz/checkbox.kt
@@ -1,11 +1,38 @@
 package com.example.miniprojectquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class checkbox : AppCompatActivity() {
+    private lateinit var comentarios: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkbox)
+
+        // Comentarios de
+        val comentarios = arrayOf(
+            "Así se hace",
+            "Estás que lo tiras",
+            "Estás hecho un verdadero Friki",
+            "Ya quedan menos",
+            "¿Seguro de tu elección?",
+            "De lo mejor que he visto",
+            "Tu si que sabes",
+            "Conoces el tema",
+            "Te ayudaría si te hiciera falta",
+            "No muy rapido pero si seguro"
+        )
+    }
+
+    fun sendMessage(vista: View){
+        Toast.makeText(applicationContext, comentarios.get((Math.random()*comentarios.length).toInt()).toString(), Toast.LENGTH_SHORT).show()
+    }
+
+    fun getResults(vista: View) {
+        val i = Intent(this, checkbox::class.java)
+        startActivity(i)
     }
 }

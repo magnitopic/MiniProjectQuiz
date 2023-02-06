d4a2ce2ced66e969a6947c8cdda541855c126509
magnitopic <magnitopic@gmail.com>
2023-02-06 12:05:26 +0100

    screen orientation

---
+++ app/src/main/java/com/example/miniprojectquiz/checkbox.kt
@@ -0,0 +1,11 @@
+package com.example.miniprojectquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class checkbox : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkbox)
+    }
+}

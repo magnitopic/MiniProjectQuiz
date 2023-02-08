5097ce4e501775ee659899463477f4ecfeae2f1d
magnitopic <magnitopic@gmail.com>
2023-02-08 15:12:59 +0100

    updated logic

--- app/src/main/java/com/example/miniprojectquiz/checkbox.kt
+++ app/src/main/java/com/example/miniprojectquiz/checkbox.kt
@@ -4,15 +4,39 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.*
 
 class checkbox : AppCompatActivity() {
-    private lateinit var comentarios: String
+    private lateinit var firstQuestionElements: Array<CheckBox>
+    private lateinit var secondQuestionElements: RadioGroup
+    private lateinit var thirdQuestionElements: RadioGroup
+    private lateinit var fourthQuestionElements: RadioGroup
+    private lateinit var fifthQuestionElements: EditText
+    private lateinit var sixthQuestionElements: Array<CheckBox>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkbox)
+        // variables para los elementos del quiz
+        firstQuestionElements = arrayOf(
+            findViewById(R.id.question1Awnser1),
+            findViewById(R.id.question1Awnser2),
+            findViewById(R.id.question1Awnser3),
+            findViewById(R.id.question1Awnser4)
+        )
+        secondQuestionElements = findViewById(R.id.radioGroup1)
+        thirdQuestionElements = findViewById(R.id.radioGroup2)
+        fourthQuestionElements = findViewById(R.id.radioGroup3)
+        fifthQuestionElements = findViewById(R.id.question5Awnser)
+        sixthQuestionElements = arrayOf(
+            findViewById(R.id.question6Awnser1),
+            findViewById(R.id.question6Awnser2),
+            findViewById(R.id.question6Awnser3),
+            findViewById(R.id.question6Awnser4)
+        )
+    }
 
-        // Comentarios de
+    // Mensajes de ánimo al usuario
+    fun sendMessage(vista: View) {
         val comentarios = arrayOf(
             "Así se hace",
             "Estás que lo tiras",
@@ -21,23 +45,39 @@ class checkbox : AppCompatActivity() {
             "¿Seguro de tu elección?",
             "De lo mejor que he visto",
             "Tu si que sabes",
-            "Conoces el tema",
-            "Te ayudaría si te hiciera falta",
-            "No muy rapido pero si seguro"
+            "Tienes bien compilado el tema",
+            "Bien hecho humano",
+            "Aún sigo siendo el androide más inteligente de la galaxia",
+            "Yo te ayudaría si te hiciera falta",
+            "Se ve que no eres el ser más rapido de la galaxia"
         )
-    }
-
-    fun sendMessage(vista: View) {
         Toast.makeText(
             applicationContext,
-            comentarios.get((Math.random() * comentarios.length).toInt()).toString(),
+            comentarios[(Math.random() * comentarios.size).toInt()].toString(),
             Toast.LENGTH_SHORT
         ).show()
     }
 
+    // Pasar a la ultima activity
     fun getResults(vista: View) {
-        val i = Intent(this, EndScreen::class.java)
-        startActivity(i)
+        // Si no se ha contestado a todas las preguntas informamos con un Toast
+        // de lo contrario calculamos score y cambiamos de activity
+        if (validateFileds()) {
+            //TODO: pass score to final screen
+            val i = Intent(this, EndScreen::class.java)
+            startActivity(i)
+        } else {
+            Toast.makeText(
+                applicationContext,
+                "Recuerda contestar todas las preguntas antes de continuar",
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 
+    fun validateFileds(): Boolean {
+
+
+        return true
+    }
 }

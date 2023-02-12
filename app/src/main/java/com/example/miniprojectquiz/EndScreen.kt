025afcf5e4c28506deeb91186a6ef93fbc4a55af
magnitopic <magnitopic@gmail.com>
2023-02-12 14:53:25 +0100

    twitter integration with initent

--- app/src/main/java/com/example/miniprojectquiz/EndScreen.kt
+++ app/src/main/java/com/example/miniprojectquiz/EndScreen.kt
@@ -4,69 +4,50 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import java.io.UnsupportedEncodingException
-import java.net.URLEncoder
 
 
 class EndScreen : AppCompatActivity() {
     private lateinit var scoreTextView: TextView
-    private lateinit var score: String
+    private var score: String? = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_end_screen)
-        val name = intent.getStringExtra("score")
+        score = intent.getStringExtra("score")
         scoreTextView = findViewById(R.id.scoreTextView)
-        scoreTextView.text = name + "/100"
+        scoreTextView.text = score + "/100"
     }
-
-    fun shareButton(vista: View) {
-        shareTwitter("He realizado el quiz de Marvin con una puntación de:\n${score}\nCrés que puedes hacerlo mejor? Descargaté la app hoy!!")
-    }
-
 
     // Código para conectarse a Twitter
-    private fun shareTwitter(message: String) {
+    fun sendTweet(view: View) {
+        val message =
+            "He realizado el quiz de Marvin con una puntación de: \n${score}/100\n Crés que puedes hacerlo mejor? Descargaté la app hoy!!"
         val tweetIntent = Intent(Intent.ACTION_SEND)
-        tweetIntent.putExtra(Intent.EXTRA_TEXT, "This is a Test.")
         tweetIntent.type = "text/plain"
-        val packManager = packageManager
-        val resolvedInfoList =
-            packManager.queryIntentActivities(tweetIntent, PackageManager.MATCH_DEFAULT_ONLY)
-        var resolved = false
-        for (resolveInfo in resolvedInfoList) {
-            if (resolveInfo.activityInfo.packageName.startsWith("com.twitter.android")) {
-                tweetIntent.setClassName(
-                    resolveInfo.activityInfo.packageName,
-                    resolveInfo.activityInfo.name
-                )
-                resolved = true
+        tweetIntent.putExtra(Intent.EXTRA_TEXT, message)
+
+        val packageManager = this.packageManager
+        val twitterApps =
+            packageManager.queryIntentActivities(tweetIntent, PackageManager.MATCH_DEFAULT_ONLY)
+
+        var twitterAppFound = false
+        for (app in twitterApps) {
+            if (app.activityInfo.packageName.startsWith("com.twitter")) {
+                tweetIntent.setPackage(app.activityInfo.packageName)
+                twitterAppFound = true
                 break
             }
         }
-        if (resolved) {
-            startActivity(tweetIntent)
+        if (twitterAppFound) {
+            this.startActivity(tweetIntent)
         } else {
-            val i = Intent()
-            i.putExtra(Intent.EXTRA_TEXT, message)
-            i.action = Intent.ACTION_VIEW
-            i.data = Uri.parse("https://twitter.com/intent/tweet?text=" + urlEncode(message))
-            startActivity(i)
-            Toast.makeText(this, "Twitter app isn't found", Toast.LENGTH_LONG).show()
-        }
-    }
-
-    // Hacer mensaje UTF-8
-    private fun urlEncode(s: String): String {
-        return try {
-            URLEncoder.encode(s, "UTF-8")
-        } catch (e: UnsupportedEncodingException) {
-            Log.d("Warning", "UTF-8 should always be supported")
-            ""
+            val webIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://twitter.com/intent/tweet?text=" + message.replace(" ", "%20"))
+            )
+            this.startActivity(webIntent)
         }
     }
 
